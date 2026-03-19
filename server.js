00444571013d347f77725efdfdede2ce886dd7f3
Lakshya Ghanghoriya <your-email@example.com>
2026-03-19 17:35:53 +0530

    withexpress

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 import crypto from "crypto";
-import { createServer } from "http";
+
 import { readFile, writeFile } from "fs/promises";
 import path from "path";
+import express from "express";
+
+
+
 const PORT = 3002;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const links_data = path.join("data", "links.json");
-const fileserver = async (res, filename, contentText) => {
-    try {
-        const data = await readFile(filename);
 
-        res.writeHead(200, { "Content-Type": contentText });
-        res.end(data);
-
-    } catch (error) {
-
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("404 Page is Unreachable");
-
-    }
-}
 
 const getLink = async () => {
     try {
@@ -33,83 +31,69 @@ const getLink = async () => {
     }
 };
 
-const server = createServer(async (req, res) => {
-
-    if (req.method === "GET") {
-
-        if (req.url === "/") {
-            return fileserver(res, path.join("public", "index.html"), "text/html")
-        }
-        else if (req.url === "/index.css") {
-            return fileserver(res, path.join("public", "index.css"), "text/css");
-
-        }
-
-        // we have create the link api becoz this link is give data to frontend so
-        //  we have to create /link which been fetched by frontend
-        else if (req.url === "/links") {
-            const links = await getLink();
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(JSON.stringify(links));
-        }
-        else if (req.url === "/favicon.ico") {
-            res.writeHead(204);
-            res.end();
-            return;
-        }
-        else {
-            const link = await getLink();
-            const shortcode = req.url.slice(1);
-            if (link[shortcode]) {
-                res.writeHead(302, { location: link[shortcode] });
-                return res.end();
-            }
-            res.writeHead(404, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify({ message: "ShortURL not Found" }));
-        }
-
-
+app.get("/", async (req, res) => {
+    try {
+        const filedata = await readFile(path.join("view", "index.html"));
+        const links = await getLink();
+        const content = filedata
+            .toString()
+            .replaceAll(
+                "{{Shorten_url}}",
+                Object.entries(links)
+                    .map(([shortcode, url]) => {
+                        return `<li>
+          <a href="/${shortcode}" target="_blank">
+            ${req.headers.host}/${shortcode}
+          </a> --- ${url}
+        </li>`;
+                    })
+                    .join("")
+            );
+        res.send(content);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal server error");
     }
-    if (req.method === "POST" && req.url === "/shorten") {
 
-        const link = await getLink();
-        let DATA = "";
-
-        req.on("data", (chunks) => {
-            DATA += chunks.toString();
-        });
-
-        req.on("end", async () => {
-            console.log(DATA);
-
-            const { url, shortcode } = JSON.parse(DATA);
-
-            if (!url) {
-                res.writeHead(400, { "Content-Type": "application/json" });
-                return res.end(JSON.stringify({ message: "Error: All requirements are not complete!" }));
-            }
-            const Finalcode = shortcode || crypto.randomBytes(4).toString("hex");
-
-            if (link[Finalcode]) {
-                res.writeHead(409, { "Content-Type": "application/json" });
-                return res.end(JSON.stringify({ message: "It already exists, use different ShortCode" }));
-            }
-
-            link[Finalcode] = url;
-            await writeFile(links_data, JSON.stringify(link, null, 2));
-
-            res.writeHead(200, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify({
-                success: true,
-                shortcode: Finalcode
-            }));
-        });
-
-        return;
+});
+app.post("/", async (req, res) => {
+    try {
+        const { url, shortcode } = req.body();
+        const links = await getLink();
+        const Finalcode = shortcode || crypto.randomBytes(4).toString("hex");
+        if (links[Finalcode]) {
+            return res.status(400).send("It already exists, use different ShortCode");
+        }
+        links[Finalcode] = url;
+        await writeFile(links_data, JSON.stringify(links, null, 2));
+        return res.send("Short URL created successfully");
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
     }
 
 });
 
-server.listen(PORT, () => {
+
+
+app.get("/:shortcode", async (req, res) => {
+    try {
+        const { shortcode } = req.params;
+        const links = await getLink();
+        if (links[shortcode]) {
+            return res.redirect(links[shortcode]);
+        } else {
+            return res.status(404).send("404!! Not Found");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server failed")
+    }
+
+});
+
+app.listen(PORT, () => {
     console.log(`Server Running at http://localhost:${PORT}`);
 });
+
+

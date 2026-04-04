d7b1edc33a398ecd9eb8d9926f7b54cfddcd707c
Lakshya Ghanghoriya <your-email@example.com>
2026-04-04 20:41:36 +0530

    comment

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ import express from "express";
 import router from "./routes/shortener.routes.js";
 
 const app = express();
-app.set("view engine", "ejs"); 
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = 3002;
 
 app.use(router);
+
+// IMPORTANT: use Render PORT
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server Running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
-
